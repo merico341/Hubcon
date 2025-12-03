65a16432038b565291ce43ad0b39b89c79fd78ae
merico341 <lonetfricoto@gmail.com>
2025-12-03 08:23:21 -0300

    fixing something again

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), netlifyPlugin()],
+  plugins: [react(), tailwindcss(), netlifyPlugin({edge: true})],
 })
