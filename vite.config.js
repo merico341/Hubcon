016acb9eb598b51122257d58cb1f3842db22175d
merico341 <lonetfricoto@gmail.com>
2025-12-03 08:18:55 -0300

    removing the other plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { reactRouter } from "@react-router/dev/vite";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), reactRouter(), netlifyPlugin()],
+  plugins: [react(), tailwindcss(), netlifyPlugin()],
 })
